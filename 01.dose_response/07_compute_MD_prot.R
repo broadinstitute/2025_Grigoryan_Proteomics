3ad818ce552ce658f3fdc239249d293a7f30e5e4
mgrigory0 <mgrigoryan@outlook.fr>
2025-07-26 15:13:16 +0200

    try

--- 01.dose_response/07_compute_MD_prot.R
+++ 01.dose_response/07_compute_MD_prot.R
@@ -1,12 +1,13 @@
-source("./Rscripts/gmd_functions.R")
+source("./01.dose_response/Rscripts/gmd_functions.R")
 
 require(dplyr)
 require(arrow)
 require(tidyr)
 require(stringr)
 
-input_file <- "../00.exploratory_analysis/inputs/proteomics_data/normalized_signal.csv"
-output_dist <- "./outputs/prot_distances.parquet"
+input_file <- "./00.exploratory_analysis/inputs/proteomics_data/mad_featselect.parquet"
+#normalized_signal.csv"
+output_dist <- "./01.dose_response/outputs/distances_test.parquet"
 
 cover_var <- 0.975
 
@@ -14,7 +15,7 @@ treatment <- "Metadata_Perturbation"
 
 # Process data
 all_dat <- read.csv(input_file) %>% as.data.frame()
-all_dat$Metadata_Perturbation <- paste(all_dat$Metadata_Compound, all_dat$Metadata_compound_concentration, sep="_")
+all_dat$Metadata_Perturbation <- paste(all_dat$Metadata_Compound, all_dat$Metadata_Concentration, sep="_")
 
 dat_cols <- colnames(all_dat)
 feat_cols <- dat_cols[!grepl("Metadata_", dat_cols)]
@@ -30,12 +31,13 @@ print("Running GMD")
 
 gmd_prep <- prep_gmd(dat, cover_var, treatment_labels)
 
-plates <- unique(all_dat$Metadata_plate_barcode)
+#plates <- unique(all_dat$Metadata_plate_barcode)
+plates <- unique(all_dat$Metadata_Plate)
 gmd_df <- data.frame()
 for (plate in plates) {
-  plate_dat <- all_dat[all_dat$Metadata_plate_barcode == plate, feat_cols]
+  plate_dat <- all_dat[all_dat$Metadata_Plate == plate, feat_cols]
   plate_dat <- as.matrix(plate_dat)
-  plate_meta <- all_dat[all_dat$Metadata_plate_barcode == plate, meta_cols]
+  plate_meta <- all_dat[all_dat$Metadata_Plate == plate, meta_cols]
   plate_labels <- plate_meta[, "Metadata_Compound"] %>% c()
 
   gmd <- compute_gmd(plate_dat, as.matrix(gmd_prep$rot),
